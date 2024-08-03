81a8b3dc7159b0e98ec2479dac31e2e4c14b0062
ravit <ravit@CRTV>
2024-08-03 20:13:04 +0530

    aug 03 2 time

---
+++ Selenium/src/java_Basics2/A_2024.java
@@ -0,0 +1,19 @@
+package java_Basics2;
+
+public class A_2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+
+	}
+
+}
